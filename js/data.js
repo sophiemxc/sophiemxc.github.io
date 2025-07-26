1180848b8c29c735c44fadcd09c884242d3d83c7
sophiemxc <sophie.mixc@gmail.com>
2025-07-26 18:44:19 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,311 @@
-
+{
+  "ARMOR": {
+    "CHAPTER 1": {
+      "Arms": [
+        "Bronze Armguard",
+        "Cotton Wristwraps",
+        "Ebongold Armguard",
+        "Folk Opera Leather Bracers",
+        "Guanyin's Prayer Beads",
+        "Pilgrim's Wristwraps",
+        "Serpentscale Bracers"
+      ],
+      "Body": [
+        "Bronze Brocade Battlerobe",
+        "Ebongold Silk Robe",
+        "Folk Opera Almsgiving Armor",
+        "Pilgrim's Garb",
+        "Serpentscale Battlerobe",
+        "Tiger Hide Loincloth"
+      ],
+      "Headgear": [
+        "Bronze Monkey Mask",
+        "Folk Opera Mask",
+        "Grey Wolf Mask",
+        "Pilgrim's Headband"
+      ],
+      "Legs": [
+        "Bronze Buskins",
+        "Cotton Legwraps",
+        "Ebongold Gaiters",
+        "Folk Opera Buskins",
+        "Pilgrim's Legwraps",
+        "Serpentscale Gaiters"
+      ]
+    },
+    "CHAPTER 2": {
+      "Arms": [
+        "Galeguard Bracers",
+        "Insect Spike Bracers",
+        "Ochre Armguard"
+      ],
+      "Body": [
+        "Galeguard Beastmaw Armor",
+        "Ochre Battlerobe"
+      ],
+      "Headgear": [
+        "Galeguard Beast Mask",
+        "Monastic Insect Hat",
+        "Snout Mask"
+      ],
+      "Legs": [
+        "Galeguard Greaves",
+        "Insect Spike Gaiters",
+        "Ochre Greaves"
+      ]
+    },
+    "CHAPTER 3": {
+      "Arms": [
+        "Iron-Tough Gauntlets",
+        "Loongscale Armguard",
+        "Vajra Armguard"
+      ],
+      "Body": [
+        "Ginseng Cape",
+        "Iron-Tough Armor",
+        "Loongscale Battlerobe"
+      ],
+      "Headgear": [
+        "Iron Horned Helm",
+        "See No Evil",
+        "Skull of Turtle Treasure"
+      ],
+      "Legs": [
+        "Iron-Tough Greaves",
+        "Loongscale Greaves"
+      ]
+    },
+    "CHAPTER 4": {
+      "Arms": [
+        "Golden Armguard",
+        "Centipede Spiked Armguard",
+        "Non-Pure Gauntlets",
+        "Venomous Armguard"
+      ],
+      "Body": [
+        "Centipede Qiang-Jin Armor",
+        "Golden Embroidered Shirt",
+        "Non-Pure Armor of Coiling Loong",
+        "Venomous Sting Insect Armor"
+      ],
+      "Headgear": [
+        "Centipede Hat of Transcendence",
+        "Golden Mask of Fury",
+        "Locust Antennae Mask",
+        "Non-Pure Broken Mask"
+      ],
+      "Legs": [
+        "Centipede Gaiters of Transcendence",
+        "Golden Greaves",
+        "Non-Pure Greaves"
+      ]
+    },
+    "CHAPTER 5": {
+      "Arms": [
+        "Bull King's Bracers",
+        "Yaksha Bracers of Outrage"
+      ],
+      "Body": [
+        "Bull King's Shan Wen Armor",
+        "Embroidered Shirt of Outrage",
+        "Yin-Yang Daoist Robe"
+      ],
+      "Headgear": [
+        "Bull King's Mask",
+        "Earth Spirit Cap",
+        "Yaksha Mask of Outrage"
+      ],
+      "Legs": [
+        "Bull King's Greaves",
+        "Yaksha Greaves of Outrage"
+      ]
+    },
+    "CHAPTER 6": {
+      "Arms": [
+        "Dian-Cui Loong-Soaring Bracers",
+        "Dian-Cui Loong-Soaring Bracers (Old Set)"
+      ],
+      "Body": [
+        "Gold Suozi Armor",
+        "Gold Suozi Armor (Old Set)"
+      ],
+      "Headgear": [
+        "Golden Feng-Tail Crown",
+        "Golden Feng-Tail Crown (Old Set)"
+      ],
+      "Legs": [
+        "Lotus Silk Cloudtreaders",
+        "Lotus Silk Cloudtreaders (Old Set)"
+      ]
+    }
+  },
+  "CHESTER": {
+    "CHAPTER 1": [
+      "Black Wind Cave - Outside the Cave",
+      "Sandgate Village - Village Entrance",
+      "Crouching Tiger Temple - Temple Entrance",
+      "Fright Cliff - Rock Clash Platform",
+      "Kingdom of Sahali - Sandgate Bound"
+    ],
+    "CHAPTER 2": [
+      "Snowhill Path - Frostclad Path",
+      "Pagoda Realm - Upper Pagoda",
+      "Pagoda Realm - Warding Temple",
+      "Bitter Lake - North Shore",
+      "New Thunderclap Temple - Entrance"
+    ],
+    "CHAPTER 3": [
+      "Village of Lanxi - Estate of the Zhu",
+      "Webbed Hollow - Upper Hollow",
+      "Webbed Hollow - Cliff of Oblivion",
+      "Temple of the Yellow Flower - Court of Illumination",
+      "Purple Cloud Mountain - Petalfall Hamlet"
+    ],
+    "CHAPTER 4": [
+      "Woods of Ember - Height of Ember",
+      "Furnace Valley - Valley Entrance",
+      "Furnace Valley - Rakshasa Palace",
+      "Furnace Valley - Emerald Hall",
+      "Field of Fire - Ashen Pass"
+    ],
+    "CHAPTER 5": [
+      "Bishui Cave - Cave Depths"
+    ]
+  },
+  "CURIO": {
+    "CHAPTER 1": [
+      "Agate Jar",
+      "Back Scratcher",
+      "Cat Eye Beads",
+      "Wind Chime"
+    ],
+    "CHAPTER 2": [
+      "Boshan Censer",
+      "Glazed Reliquary",
+      "Goldflora Hairpin",
+      "Tiger Tally",
+      "Tridacna Pendant"
+    ],
+    "CHAPTER 3": [
+      "Auspicious Lantern",
+      "Beast Buddha",
+      "Bronze Buddha Pendant",
+      "Fine China Tea Bowl",
+      "Frostsprout Twig"
+    ],
+    "CHAPTER 4": [
+      "Celestial Registry Tablet",
+      "Cuo Jin-Yin Belt Hook",
+      "Gold Sun Crow",
+      "Golden Carp",
+      "Jade Moon Rabbit"
+    ],
+    "CHAPTER 5": [
+      "Amber Prayer Beads",
+      "Daoist's Basket of Fire and Water",
+      "Flame Orb",
+      "Gold Button",
+      "White Seashell Waist Chain"
+    ],
+    "CHAPTER 6": [
+      "Celestial Birthstone Fragment",
+      "Waterward Orb"
+    ]
+  },
+  "GOURD": {
+    "CHAPTER 1": [
+      "Old Gourd",
+      "Trailblazer's Scarlet Gourd",
+      "Plaguebane Gourd",
+      "Immortal Blessing Gourd"
+    ],
+    "CHAPTER 2": [
+      "Jade Lotus Gourd",
+      "Fiery Gourd",
+      "Stained Jade Gourd",
+      "Xiang River Goddess Gourd"
+    ],
+    "CHAPTER 3": [
+      "Multi-Glazed Gourd",
+      "Qing-Tian Gourd"
+    ]
+  },
+  "MEDICINE": {
+    "CHAPTER 1": [
+      "Ascension Powder",
+      "Essence Decoction",
+      "Evil Repelling Medicament",
+      "Longevity Decoction"
+    ],
+    "CHAPTER 2": [
+      "Enhanced Tiger Subduing Pellets",
+      "Fortifying Medicament",
+      "Loong Aura Amplification Pellets",
+      "Tonifying Decoction"
+    ],
+    "CHAPTER 3": [
+      "Enhanced Ginseng Pellets",
+      "Life-Saving Pill",
+      "Mirage Pill",
+      "Septenary Heartfire Pill"
+    ]
+  },
+  "RELIC": [
+    "Formula",
+    "Seed"
+  ],
+  "SPELL": {
+    "CHAPTER 1": [
+      "Cloud Step",
+      "Immobilize",
+      "Spell Binder"
+    ],
+    "CHAPTER 2": [
+      "A Pluck of Many",
+      "Ring of Fire",
+      "Rock Solid"
+    ],
+    "CHAPTER 3": [
+      "Stone Monkey: Azure Dome",
+      "Yin Tiger: Ebon Flow"
+    ]
+  },
+  "SPIRIT": {
+    "CHAPTER 1": [
+      "Baw-Li-Guhh-Lang",
+      "Guangmou",
+      "Wolf Guai: Red Tides"
+    ],
+    "CHAPTER 2": [
+      "Gore-Eye Daoist",
+      "Mad Tiger",
+      "Poisestone",
+      "Rat Archer"
+    ],
+    "CHAPTER 3": [
+      "Blade Monk",
+      "Clay Vajra",
+      "Crow Diviner"
+    ]
+  },
+  "UPGRADE_ITEM": {
+    "CHAPTER 1": [
+      "Fireproof Mantle",
+      "Luojia Fragrant Vine",
+      "Wind Tamer"
+    ],
+    "CHAPTER 2": [
+      "Celestial Pill",
+      "Jade Lotus",
+      "Nonary"
+    ],
+    "CHAPTER 3": [
+      "Shen Monkey",
+      "Taiyi",
+      "Weaver's Needle"
+    ]
+  },
+  "VESSEL": [
+    "Awaken Wine Worm"
+  ]
+}
